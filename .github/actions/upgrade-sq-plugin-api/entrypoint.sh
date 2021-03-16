63a0f0240530368d20649ecaefa76e842868eb55
David Rautureau <david.rautureau@sonarsource.com>
2021-03-16 16:29:26 +0100

    Test upgrade SQ plugin API

---
+++ .github/actions/upgrade-sq-plugin-api/entrypoint.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Set committer identity
+github_user="drautureau-sonarsource"
+git config --global user.email "${github_user}@sonarsource.com"
+git config --global user.name "${github_user}"
+
+echo "Checkout the branch 'check-update-of-sq-plugin-api'"
+git fetch origin check-update-of-sq-plugin-api
+git checkout check-update-of-sq-plugin-api
+
+echo "Rebase on master"
+git rebase master
+
+echo "Get the lastest master version of the SQ plugin API"
+group_id="org.sonarsource.sonarqube"
+artifact_id="sonar-plugin-api"
+version=$(curl -s -u"${ARTIFACTORY_USERNAME}:${ARTIFACTORY_PASSWORD}" \
+          "${ARTIFACTORY_URL}/api/search/latestVersion?g=${group_id}&a=${artifact_id}&repos=sonarsource-builds")
+echo "Lastest master version of the SQ plugin API is ${version}"
+
+if grep -Fq "${group_id}:${artifact_id}:${version}" build.gradle
+then
+  echo "Lastest master version of the SQ plugin API is already used. Nothing to do then!"
+  exit 0
+else
+  echo "Upgrade the dependency with the new version"
+  sed -i "s/${group_id}:${artifact_id}:.*'/${group_id}:${artifact_id}:${version}'/g" build.gradle
+
+  echo "Commit and push the change"
+  git commit -am "Upgrade SQ plugin API to version $version"
+
+  echo "Push force (kindly)"
+  git push --force-with-lease
+fi
